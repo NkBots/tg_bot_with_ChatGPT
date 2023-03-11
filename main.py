b0781d44b41f73c84721207752bd40ec8b431d18
NkBots <116135526+NkBots@users.noreply.github.com>
2023-03-11 14:53:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,38 +4,42 @@ from aiogram import Bot, Dispatcher
 from aiogram.utils import executor
 from settings import API_KEY, BOT_KEY
 
-# Логгирование
+
 logging.basicConfig(level=logging.INFO)
 
-# Устанавливаем API_KEY для OpenAI
+
 openai.api_key = API_KEY
 
-# Токен для Telegram бота
+
 bot = Bot(token=BOT_KEY)
 
-# Диспетчер
+
 dp = Dispatcher(bot)
 
-# История сообщений
+
 contex_history = {}
 
-# Модель обработки данных в ChatGPT
+
 model = 'text-davinci-003'
 
 
-# Хэндлер на команду /start
-@dp.message_handler(commands=['start'])
-async def start(message):
-    if message['language_code'] == 'en':
-        await bot.send_message(
-            chat_id=message.from_user.id,
-            text=f'Привет {message.from_user.first_name}. Я отвечу практически на любой твой вопрос. Что ты хочешь узнать?'
-        )
-    else:
-        await bot.send_message(
-            chat_id=message.from_user.id,
-            text=f'Hello {message.from_user.first_name}. I will answer almost any of your questions. What do you want to know?'
-        )
+
+@dp.message_handler(commands=['help'])
+def help(message):
+  bot.send_message(message.chat.id, f"""This bot has two models. Currently using the {current_model} model.
+  
+  dav3 is a weaker language model. She also doesn't remember dialogue. gpt3.5 is the model currently used in ChatGPT. Also, this model remembers the dialogue, which makes it an indispensable assistant.
+  
+  To change the model type /mode followed by gpt3.5 or dav3
+  
+  For dev3, there is a temperature setting (/temp) from 0 to 1. The higher the temperature, the less formal text the model produces. If the value is high, there may be factual errors!""")
+
+
+@bot.message_handler(commands=['start'])
+def help(message):
+  bot.send_message(message.chat.id, f"""I am AI Bot Developed By @NitinSahay for private use Only""")
+
+
 
 
 @dp.message_handler(lambda message: True)
@@ -43,49 +47,47 @@ async def handle_message(message):
     # id пользователя
     user_id = message.chat.id
 
-    # Сообщение при повторной отправке запроса
-    spam = 'Не надо спамить, просто немного подожди. Запрос обрабатывается...' \
-        if message.from_user.language_code == 'en' \
-        else 'Don`t spam, just relax and wait...'
+    
+    spam = 'Dont Spam please wait 250 sec'
 
-    # Проверяем есть были ли сообщения от пользователя
+   
     if contex_history.get(user_id) == None:
 
-        # Запрос данных и добавление сообщения в историю
+        
         response = await generate_response(message.text)
 
-        # Сохраняем в историю текст сообщения и дату запроса
+       
         contex_history[user_id] = [message.text]
     else:
 
         contex_history[user_id] += [message.text]
         len_history = len(contex_history[user_id])
 
-        # Проверяем на спам - повторная отправка сообщения
+        
         if len_history > 1:
 
-            # Проверяем, что в прошлый раз не было такого же сообщения
+            
             if contex_history[user_id][-1].lower().strip() == contex_history[user_id][-2].lower().strip():
                 del contex_history[user_id][-1]
                 await bot.send_message(chat_id=user_id, reply_to_message_id=message.message_id, text=spam)
                 return
 
-                # Проверяем, что в истории не более 10 сообщений, если больше, то удаляем самое старое
+                
         if len(contex_history[user_id]) > 9:
             del contex_history[user_id][0]
 
-        # Получаем ответ
+        
         response = await generate_response(message.text + ' ' + '\n'.join(contex_history.get(user_id)))
 
     if response['text']:
         await bot.send_message(chat_id=user_id, reply_to_message_id=message.message_id, text=response['text'])
 
 
-# Генерируем ответ на основе входящего текста и текущего контекста диалога
+
 async def generate_response(text):
     prompt = f"{text}"
     max_tokens = 1024
-    # Запрос
+    
     response = openai.Completion.create(
         model=model,
         prompt=prompt,
