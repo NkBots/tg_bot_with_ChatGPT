ce7b3114aa9ee0c773e6f29762db6d830f5269d8
NkBots <116135526+NkBots@users.noreply.github.com>
2023-03-11 15:20:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,14 +34,14 @@ current_model = 'gpt3.5'
 
 @dp.message_handler(commands=['help'])
 async def help(message):
-  bot.send_message(message.chat.id, f"""This bot has two models. dav3 is a weaker language model. She also doesn't remember dialogue. gpt3.5 is the model currently used in ChatGPT. Also, this model remembers the dialogue, which makes it an indispensable assistant.
+  await bot.send_message(message.chat.id, f"""This bot has two models. dav3 is a weaker language model. She also doesn't remember dialogue. gpt3.5 is the model currently used in ChatGPT. Also, this model remembers the dialogue, which makes it an indispensable assistant.
   
   To change the model type /mode followed by gpt3.5 or dav3""")
 
 
 @dp.message_handler(commands=['start'])
 async def help(message):
-  bot.send_message(message.chat.id, f"""I am AI Bot Developed By @NitinSahay for private use Only""")
+  await bot.send_message(message.chat.id, f"""I am AI Bot Developed By @NitinSahay for private use Only""")
 
 
 
