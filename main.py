b774f60f25103af8b7e9eb1c3bed7ec565071138
NkBots <116135526+NkBots@users.noreply.github.com>
2023-03-11 15:11:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,13 +34,9 @@ current_model = 'gpt3.5'
 
 @dp.message_handler(commands=['help'])
 def help(message):
-  bot.send_message(message.chat.id, f"""This bot has two models. Currently using the {current_model} model.
+  bot.send_message(message.chat.id, f"""This bot has two models. dav3 is a weaker language model. She also doesn't remember dialogue. gpt3.5 is the model currently used in ChatGPT. Also, this model remembers the dialogue, which makes it an indispensable assistant.
   
-  dav3 is a weaker language model. She also doesn't remember dialogue. gpt3.5 is the model currently used in ChatGPT. Also, this model remembers the dialogue, which makes it an indispensable assistant.
-  
-  To change the model type /mode followed by gpt3.5 or dav3
-  
-  For dev3, there is a temperature setting (/temp) from 0 to 1. The higher the temperature, the less formal text the model produces. If the value is high, there may be factual errors!""")
+  To change the model type /mode followed by gpt3.5 or dav3""")
 
 
 @dp.message_handler(commands=['start'])
@@ -49,34 +45,4 @@ def help(message):
 
 
 
-messages = [{"role": "system", "content": "You are a helpful assistant."},]
 
-@dp.message_handler(func=lambda _:True)
-def handle_message(message):
-  if current_model == 'gpt3.5':
-    message_dict = {"role": "user", "content": message.text }
-    messages.append(message_dict)
-    response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo", 
-      messages=messages
-    )
-    reply = response.choices[0].message.content #response['choices'][0]['text']
-    bot.send_message(chat_id=message.from_user.id,text=reply)
-    messages.append({"role": "assistant", "content": reply})
-  elif current_model == 'dav3':
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=message.text, 
-    temperature=temp,       
-    max_tokens=1000,       
-    top_p=1,                
-    frequency_penalty=0.0,  
-    presence_penalty=0.6,   
-    stop=[" Human:", " AI:"]
-  )
-    reply = response['choices'][0]['text']
-   # bot.send_message(chat_id=message.from_user.id,text=reply)
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp)
